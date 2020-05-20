a3e7ea15ab50281e603ce6d507e11fee26b7dc66
michaelberg1005 <mh.berg@comcast.net>
2020-05-20 15:27:34 -0700

    adding filtered table

--- js/app.js
+++ js/app.js
@@ -24,3 +24,10 @@ function buildTable(data) {
     );
   });
 }
+
+function handleClick() {
+    let date = d3.select("#datetime").property("value");
+    let filteredData = tableData;
+    if (date) {
+        filteredData = filteredData.filter(row => row.datetime === date);
+    };
